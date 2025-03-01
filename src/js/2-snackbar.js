04b8b5282cd8eb0d3001b735c4bb3f7a2729d52e
Andrii Pichkorskiy <desertptu4ka@gmail.com>
2025-03-01 18:50:19 +0200

    Task complited

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+/**
+ * @param {SubmitEvent} event
+ */
+function createPromise(event) {
+  event.preventDefault();
+  const state = event.target.state.value;
+  const delay = event.target.delay.value;
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') resolve();
+      else reject();
+    }, delay);
+  })
+    .then(() =>
+      iziToast.success({
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      })
+    )
+    .catch(() =>
+      iziToast.error({
+        position: 'topRight',
+        message: `❌ Rejected promise in ${delay}ms`,
+      })
+    );
+}
+
+form.addEventListener('submit', createPromise);
