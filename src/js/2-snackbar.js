9a1ec0a0e220aee6e60eaaddfb9435474c4d2943
Andrii Pichkorskiy <desertptu4ka@gmail.com>
2025-03-02 19:50:13 +0200

    Added converting input value to number

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -10,7 +10,24 @@ const form = document.querySelector('.form');
 function createPromise(event) {
   event.preventDefault();
   const state = event.target.state.value;
-  const delay = event.target.delay.value;
+  const delay = Number(event.target.delay.value);
+
+  if (isNaN(delay) || delay === Infinity || delay === -Infinity) {
+    iziToast.warning({
+      position: 'topRight',
+      message: `Value ${delay} is not a valide number, please do no change value, input type and attribute "required" in the code inspector`,
+    });
+    return;
+  }
+
+  if (delay < 0) {
+    iziToast.warning({
+      position: 'topRight',
+      message: 'Please enter a positive number',
+    });
+    return;
+  }
+
   new Promise((resolve, reject) => {
     setTimeout(() => {
       if (state === 'fulfilled') resolve(delay);
